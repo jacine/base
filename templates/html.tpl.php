6eca5a24aa9955c2a1c3b850caa7b87dc0607eb3
Jacine Luisi <jacine.luisi@gmail.com>
2012-07-04 21:04:32 -0400

    Add meta tags to base.

--- templates/html.tpl.php
+++ templates/html.tpl.php
@@ -3,7 +3,6 @@
   <head>
   <?php print $head; ?>
   <title><?php print $head_title; ?></title>
-  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
   <?php print $styles; ?>
   <?php print $scripts; ?>
   <!--[if lt IE 9]>
